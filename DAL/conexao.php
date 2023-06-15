0bbe8e22eaaffa38242353057ae13897137f8a26
ericksikina <erickduartesikina@gmail.com>
2023-06-15 20:27:44 -0300

    Update conexao.php

--- DAL/conexao.php
+++ DAL/conexao.php
@@ -1,14 +1,38 @@
 <?php
 
-$usuario = 'root';
-$senha = '';
-$database = 'loja';
-$host = 'localhost';
+namespace DAL;
 
-$mysqli = new mysqli($host, $usuario, $senha, $database);
+use PDO;
 
-if($mysqli->error) {
-    die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+class Conexao{
+    private static $dbNome='loja'; 
+    private static $dbHost = 'localhost';
+    private static $dbUsuario = 'root'; 
+    private static $dbSenha = '';  
+
+    private static $cont = null; 
+
+    public function __construct()
+    {
+        die("A função init não é permitida"); 
+    }
+
+    public static function conectar(){
+        if (self::$cont == null){
+            try{
+               self::$cont = new  \PDO("mysql:host=". self::$dbHost .";dbname=" . self::$dbNome , self::$dbUsuario, self::$dbSenha);
+            }
+            catch (\PDOException $exception) {
+                die ($exception->getMessage());
+            }
+ 
+        }
+        return self::$cont; 
+    }
+
+    public static function desconectar (){
+        self::$cont = null; 
+    }
+
 }
-
 ?>
