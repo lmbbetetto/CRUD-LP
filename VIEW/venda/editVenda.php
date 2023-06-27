00aeaeb5ba9a45c1a5b764841f1f8775ff4a4a95
ericksikina <erickduartesikina@gmail.com>
2023-06-27 16:41:19 -0300

    Title

--- VIEW/venda/editVenda.php
+++ VIEW/venda/editVenda.php
@@ -17,7 +17,7 @@ $venda = $bll->SelectID($id);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar venda</title>
+    <title>Alterar Venda</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/funcionario.css">
--- VIEW/venda/lsVenda.php
+++ VIEW/venda/lsVenda.php
@@ -29,7 +29,7 @@ else
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista Vendas</title>
+    <title>Listar Vendas</title>
     <link rel="stylesheet" href="../css/reset.css">
     <link rel="stylesheet" href="../css/lsfunc.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
